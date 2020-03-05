1826db7ee255a405a45f9b42bfcaa5fc007447d2
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-05 15:15:56 +0400

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  let res;
+  !array || array.length === 0  ? res = 0 : res = Math.min(...array)
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let res;
+  !array || array.length === 0  ? res = 0 : res = Math.max(...array)
+  return res;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let res;
+  if (!array || array.length === 0) {
+    res = 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    res = sum / array.length
+  }
+  return res;
+
 }
